8a3ff25bc458cc8ea68320070c998607770c689d
Ridoy-MAX <pou4w98y@gmail.com>
2023-05-31 01:53:12 +0600

    booking page added .

--- js/custom.js
+++ js/custom.js
@@ -11,8 +11,15 @@ window.onload = function(){
 
 
 
+  let cursor = document.getElementById("cursor");
 
-
+  document.addEventListener("mousemove", (e) => {
+    let x = e.clientX;
+    let y = e.clientY;
+    cursor.style.left = x + "px";
+    cursor.style.top = y + "px";
+  });
+  
 
 
 // $(document).ready(function(){ 
